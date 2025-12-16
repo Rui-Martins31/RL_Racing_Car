10dfd7e024f229997ab3f3371f2f9226096b7898
Rui Martins <ruimartins203@gmail.com>
2025-12-16 12:51:54 +0000

    Simple control placeholder

--- control.cpp
+++ control.cpp
@@ -1,6 +1,20 @@
 #include <stdio.h>
 
-int main()
+#include "control.hpp"
+
+MessageClient control(MessageServer message)
 {
-    
+    // Control struct
+    MessageClient control;
+
+    // Placeholder
+    control.accel = 1.0;
+    control.brake = 0.0;
+    control.clutch = 0.0;
+    control.focus = 0.0;
+    control.gear = 1;
+    control.meta = false;
+    control.steering = 0.0;
+
+    return control;
 }
---
+++ control.hpp
@@ -0,0 +1,9 @@
+#ifndef CONTROL_HPP
+#define CONTROL_HPP
+
+#include "utils/parser.hpp"
+
+// Control based on the current sensor values
+MessageClient control(MessageServer message);
+
+#endif
